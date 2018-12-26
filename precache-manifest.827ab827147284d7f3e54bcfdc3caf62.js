8920e6856e9062be4249c69dd93bf0502c0f1ecd
mhrice <matthewrice11@yahoo.com>
2018-12-25 23:38:07 -0500

    Updates

---
+++ precache-manifest.827ab827147284d7f3e54bcfdc3caf62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "738a16a178d39df74782",
+    "url": "/sudoku/static/js/runtime~main.738a16a1.js"
+  },
+  {
+    "revision": "69dfad544852313fec27",
+    "url": "/sudoku/static/js/main.69dfad54.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/sudoku/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "69dfad544852313fec27",
+    "url": "/sudoku/static/css/main.a2975ca4.chunk.css"
+  },
+  {
+    "revision": "75cfe2f5ef7854b4ac7c4f942a719471",
+    "url": "/sudoku/index.html"
+  }
+];
